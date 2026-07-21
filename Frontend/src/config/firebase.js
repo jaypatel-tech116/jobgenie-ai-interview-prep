ad2eff2f72deb40a719170a24e24f450f2ee2829
jaypatel-tech116 <176357721+jaypatel-tech116@users.noreply.github.com>
2026-07-21 17:32:50 +0530

    chore: update Firebase configuration

--- Frontend/src/config/firebase.js
+++ Frontend/src/config/firebase.js
@@ -2,10 +2,17 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyBU4jl_uDC3yYps9Ijo0BK5fv0DnYeMmU8",
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "jobgenie---an-interview-ai.firebaseapp.com",
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "jobgenie---an-interview-ai",
-  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:166590211912:web:d08bf35716a42a09bfb57d",
+  apiKey:
+    import.meta.env.VITE_FIREBASE_API_KEY ||
+    "AIzaSyCLx3zaV16tg40CdG9OVbFOMxqODcYWe4I",
+  authDomain:
+    import.meta.env.VITE_FIREBASE_AUTH_DOMAIN ||
+    "jobgenie-ai-interview.firebaseapp.com",
+  projectId:
+    import.meta.env.VITE_FIREBASE_PROJECT_ID || "jobgenie-ai-interview",
+  appId:
+    import.meta.env.VITE_FIREBASE_APP_ID ||
+    "1:9652880283:web:c3bde552682ff54511973e",
 };
 
 const app = initializeApp(firebaseConfig);
